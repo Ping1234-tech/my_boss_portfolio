70847bd40b86b879a3034ca721f015544e93b187
Ping1234-tech <smithcomfort251@gmail.com>
2025-12-13 05:34:44 +0000

    Set up Tailwind CSS

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Hero from './component/Hero/hero.jsx'
+import Nav from './component/Nav/nav.jsx'
+
+
+
+function App() {
+  
+  return (
+    <>
+      <Nav />
+      <Hero />
+    </>
+  )
+}
+
+export default App
